83e81769df65551422fa3897b87924407ae7eafb
vmanriquec <pool_rodrigo1@yahoo.com>
2020-12-01 02:35:56 -0500

    segundo 4

--- app/src/main/java/com/driversuperdelivery/driversuperdelivery/adaptadores/Adaptadorrecibepedidos.java
+++ app/src/main/java/com/driversuperdelivery/driversuperdelivery/adaptadores/Adaptadorrecibepedidos.java
@@ -32,8 +32,13 @@ import com.driversuperdelivery.driversuperdelivery.modelos.CremaRealm;
 import com.driversuperdelivery.driversuperdelivery.modelos.CremaRealmFirebase;
 import com.driversuperdelivery.driversuperdelivery.modelos.DetallepedidoRealmFirebase;
 import com.driversuperdelivery.driversuperdelivery.modelos.Detallepedidorealm;
+import com.driversuperdelivery.driversuperdelivery.modelos.Empresa;
 import com.driversuperdelivery.driversuperdelivery.modelos.PedidoRealmFirebase;
 import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -45,6 +50,7 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.DataTruncation;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -93,7 +99,7 @@ protected ToggleButton togle;
             this.vuelto=(TextView) v.findViewById(R.id.vuelto);
             this.idpedido=(TextView) v.findViewById(R.id.idpedido);
             this.wasap=(Button) v.findViewById(R.id.watsapp);
-            this.rechazarpedido=(Button) v.findViewById(R.id.rechazarorden);
+
             this.fechitapedido=(TextView) v.findViewById(R.id.fechitapedido);
 
 this.muestrapedido=(Button)v.findViewById(R.id.muestrapedido);
@@ -116,7 +122,7 @@ this.muestrapedido=(Button)v.findViewById(R.id.muestrapedido);
         final PedidoRealmFirebase item = items.get(position);
         viewHolder.itemView.setTag(item);
         viewHolder.ordenentregada.setVisibility(View.VISIBLE);
-        viewHolder.rechazarpedido.setVisibility(View.VISIBLE);
+
         viewHolder.vuelto.setText(String.valueOf(item.getVuelto()));
         viewHolder.cuantopaga.setText(String.valueOf(item.getCuantopagaecliente()));
         viewHolder.referencias.setText(String.valueOf(item.getReferencias()));
@@ -132,7 +138,14 @@ viewHolder.fechitapedido.setText(String.valueOf(item.getFechapedido()));
         Picasso.get().load(String.valueOf(item.getIdfacebook())).transform(new CropSquareTransformation()).resize(400, 400).into(viewHolder.local);
 
 
+viewHolder.local.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        String hh=item.getIdempresa().toString();
+        new trardatosdeempresa().execute(hh);
 
+    }
+});
 viewHolder.togle.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View v) {
@@ -165,21 +178,17 @@ if (item.getDescripcionpedido().toString().equals("null")){
                 //stopButton.setVisibility(View.VISIBLE);
 
                 viewHolder.ordenentregada.setVisibility(View.GONE);
-                viewHolder.rechazarpedido.setVisibility(View.GONE);
-
                 break;
             case "rechazado":
 
                 viewHolder.estadopedido.setBackgroundColor(R.color.colorPrimary);
                 viewHolder.estadopedido.setText("Rechazado");
                 viewHolder.ordenentregada.setVisibility(View.GONE);
-                viewHolder.rechazarpedido.setVisibility(View.GONE);
                 break;
             case "generadodelivery":
                 viewHolder.estadopedido.setBackgroundColor(R.color.chatotro);
                 viewHolder.estadopedido.setText("Por entregar");
                 viewHolder.ordenentregada.setVisibility(View.VISIBLE);
-                viewHolder.rechazarpedido.setVisibility(View.VISIBLE);
                 break;
 
             default:
@@ -329,17 +338,6 @@ todoslosdetalles.clear();
     }
 });
 
-viewHolder.rechazarpedido.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        String op=viewHolder.idpedido.getText().toString();
-        new rechazapedido().execute(op);
-        items.remove(position);
-        notifyItemRemoved(position);
-        notifyItemRangeChanged(position, getItemCount());
-
-    }
-});
 
 
         viewHolder.wasap.setOnClickListener(new View.OnClickListener() {
@@ -371,92 +369,6 @@ viewHolder.rechazarpedido.setOnClickListener(new View.OnClickListener() {
     }
 
 
-
-    private class rechazapedido extends AsyncTask<String, String, String> {
-        HttpURLConnection conne;
-//        ProgressDialog pdLoading = new ProgressDialog(mainContext.this);
-        URL url = null;
-        ArrayList<Adicional> listadeadicionales = new ArrayList<Adicional>();
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-  //          pdLoading.setMessage("\tCargando Adicionales");
-    //        pdLoading.setCancelable(false);
-      //      pdLoading.show();
-        }
-        @Override
-        protected String doInBackground(String... params) {
-            try {
-                url = new URL("https://sodapop.pe/sugest/apirechazarpedido.php");
-            } catch (MalformedURLException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-                return e.toString();
-            }
-            try {
-                conne = (HttpURLConnection) url.openConnection();
-                conne.setReadTimeout(READ_TIMEOUT);
-                conne.setConnectTimeout(CONNECTION_TIMEOUT);
-                conne.setRequestMethod("POST");
-                conne.setDoInput(true);
-                conne.setDoOutput(true);
-                Uri.Builder builder = new Uri.Builder()
-                        .appendQueryParameter("idpedido", params[0]);
-                String query = builder.build().getEncodedQuery();
-                // Open connection for sending data
-                OutputStream os = conne.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(
-                        new OutputStreamWriter(os, "UTF-8"));
-                writer.write(query);
-                writer.flush();
-                writer.close();
-                os.close();
-                conne.connect();
-            } catch (IOException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-                return e1.toString();
-            }
-            try {
-                int response_code = conne.getResponseCode();
-                if (response_code == HttpURLConnection.HTTP_OK) {
-                    InputStream input = conne.getInputStream();
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-                    StringBuilder result = new StringBuilder();
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        result.append(line);
-                    }
-                    return (
-
-                            result.toString()
-
-                    );
-                } else {
-                    return ("Connection error");
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-                return e.toString();
-
-            } finally {
-                conne.disconnect();
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            Log.d("paso",result.toString());
-        //    pdLoading.dismiss();
-
-            if (result.equals("no rows")) {
-            } else {
-
-            }
-        }
-
-    }
 
     private class entregarpedido extends AsyncTask<String, String, String> {
         HttpURLConnection conne;
@@ -719,6 +631,173 @@ viewHolder.rechazarpedido.setOnClickListener(new View.OnClickListener() {
         }
 
     }
+    private class trardatosdeempresa extends AsyncTask<String, String, String> {
 
+        HttpURLConnection conne;
+        URL url = null;
+        ArrayList<Empresa> listaalmaceno = new ArrayList<Empresa>();
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                url = new URL("https://sodapop.pe/sugest/traerdatitodelaempresa.php");
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return e.toString();
+            }
+            try {
+                conne = (HttpURLConnection) url.openConnection();
+                conne.setReadTimeout(READ_TIMEOUT);
+                conne.setConnectTimeout(CONNECTION_TIMEOUT);
+                conne.setRequestMethod("POST");
+                conne.setDoInput(true);
+                conne.setDoOutput(true);
+                // Append parameters to URL
+                Uri.Builder builder = new Uri.Builder()
+                        .appendQueryParameter("idempresa", params[0]);
+                String query = builder.build().getEncodedQuery();
+
+                // Open connection for sending data
+                OutputStream os = conne.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+                conne.connect();
+
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+                return e1.toString();
+            }
+            try {
+                int response_code = conne.getResponseCode();
+                if (response_code == HttpURLConnection.HTTP_OK) {
+                    InputStream input = conne.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+
+                    }
+                    return (
+
+                            result.toString()
+
+
+                    );
+
+                } else {
+                    return ("Connection error");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+
+                return e.toString();
+            } finally {
+                conne.disconnect();
+            }
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            String[] strArrData = {"No Suggestions"};
+            ArrayList<Empresa> people = new ArrayList<>();
+
+            people.clear();
+            RecyclerView.Adapter adapter;
+
+
+            ArrayList<String> dataList = new ArrayList<String>();
+            Empresa meso;
+            if (result.equals("no rows")) {
+
+
+            } else {
+                try {
+                    JSONArray jArray = new JSONArray(result);
+                    for (int i = 0; i < jArray.length(); i++) {
+                        JSONObject json_data = jArray.optJSONObject(i);
+                        meso = new Empresa(
+
+
+                                json_data.getInt("idempresa")
+                                ,json_data.getString("razonsocialempresa")
+                                ,json_data.getString("direccionempresa")
+                                , json_data.getString("telefonoempresa")
+                                ,json_data.getString("idhorarioempresa")
+                                ,json_data.getString("emailempresa")
+                                ,json_data.getString("paginawebempresa")
+                                ,json_data.getString("estadoempresa")
+                                ,json_data.getString("sloganempresa")
+                                ,json_data.getString("nombreadministrador")
+                                ,json_data.getString("telefonoadministrador")
+                                , json_data.getString("logotipoempresa")
+                                ,json_data.getString("idrubroempresa")
+                                ,json_data.getString("tiempodedemoraempresa")
+                                ,json_data.getString("costodelivery")
+                                ,json_data.getString("latitudempresa")
+                                ,json_data.getString("longitudempresa"))
+
+                                ;
+                        people.add(meso);
+                    }
+                    final Dialog dialog = new Dialog(mainContext);
+                    dialog.setTitle("Details");
+
+                    dialog.setContentView(R.layout.dialogolocal);
+
+                    TextView nombie=(TextView) dialog.findViewById(R.id.nombreempre);
+ImageView fot=(ImageView) dialog.findViewById(R.id.imageView8);
+                    Button was=(Button)dialog.findViewById(R.id.wasapeada);
+                    Button direcc=(Button)dialog.findViewById(R.id.direccionempresa);
+
+                    nombie.setText(people.get(0).getRazonsocialempresa());
+                    TextView dirio=(TextView) dialog.findViewById(R.id.dirio);
+                    dirio.setText(people.get(0).getDireccionempresa());
+                    Picasso.get().load(String.valueOf(String.valueOf(people.get(0).getIdrubroempresa()))).transform(new CropSquareTransformation()).resize(400, 400).into(fot);
+
+
+
+
+                    Button w=(Button)dialog.findViewById(R.id.wasapeada);
+                    w.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            try {
+                                Intent intent = new Intent(Intent.ACTION_VIEW);
+                                intent.setData(Uri.parse("http://api.whatsapp.com/send?phone="+"+51"+people.get(0).getTelefonoempresa().toString()+"&text="+"Hola, soy el driver que llevara tu pedido llegare en "));
+                                mainContext.startActivity(intent);
+                            }catch (Exception e){
+                                e.printStackTrace();
+                            }
+
+                        }
+                    });
+                    dialog.show();
+
+
+
+                } catch (JSONException e) {
+
+                }
+            }
+        }
+    }
 }
 
